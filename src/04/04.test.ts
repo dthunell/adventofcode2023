2fbd43c6fd5f34213cfc1e43c872099c55a6b85d
Daniel Nymberg Thunell <daniel.nymberg-thunell@folksam.se>
2023-12-05 12:18:10 +0100

    04

--- src/04/04.test.ts
+++ src/04/04.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from '@jest/globals';
-import { a, b, rowToCard, pointsOnCard } from './04'
+import { a, b, rowToCard, pointsOnCard, matchesOnCard, getNextCards } from './04'
 
 describe('04', () => {
     describe('helper', () => {
@@ -19,6 +19,30 @@ describe('04', () => {
             const point = pointsOnCard(card)
             expect(point).toBe(8)
         })
+        it('should be have 4 matches', () => {
+            const row = 'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53'
+            const card = rowToCard(row)
+            const point = matchesOnCard(card)
+            expect(point).toBe(4)
+        })
+        
+        it('should return next 2 cards', () => {
+            const row1 = 'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53'
+            const row2 = 'Card 2: 41 48 83 86 17 | 83 86  6 31 17  9 48 53'
+            const row3 = 'Card 3: 41 48 83 86 17 | 83 86  6 31 17  9 48 53'
+            const cards = [rowToCard(row1), rowToCard(row2), rowToCard(row3) ]
+            const nextCards = getNextCards(cards, 0, 2)
+            expect(nextCards.length).toBe(2)
+        })
+
+        it('should return last card', () => {
+            const row1 = 'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53'
+            const row2 = 'Card 2: 41 48 83 86 17 | 83 86  6 31 17  9 48 53'
+            const row3 = 'Card 3: 41 48 83 86 17 | 83 86  6 31 17  9 48 53'
+            const cards = [rowToCard(row1), rowToCard(row2), rowToCard(row3) ]
+            const nextCards = getNextCards(cards, 1, 2)
+            expect(nextCards.length).toBe(1)
+        })
     })
 
     describe('A', () => {
@@ -28,10 +52,10 @@ describe('04', () => {
         })
     })
 
-    // describe.skip('B', () => {
-    //     it('should return 467835 for sample', () => {
-    //         const result = b('03/b.sample.txt')
-    //         expect(result).toBe(467835)
-    //     })
-    // })
+    describe('B', () => {
+        it('should return 30 for sample', () => {
+            const result = b('04/b.sample.txt')
+            expect(result).toBe(30)
+        })
+    })
 })
--- src/04/04.ts
+++ src/04/04.ts
@@ -26,24 +26,85 @@ const pointsOnCard = (card: Card): number => {
     if (points.length === 0) {
         return 0
     }
-    
+
     return points.reduce((m, n) => m * n)
+}
+
+const matchesOnCard = (card: Card): number => {
+    let matches = 0
+    for (const w of card.winning) {
+        if (card.numbers.includes(w)) {
+            matches++
+        }
+    }
+
+    return matches
+}
+
+const getNextCards = (cards: Card[], currentIndex: number, numCards: number): Card[] => {
+    const nextIndex = currentIndex + 1
+    return cards.slice(nextIndex, nextIndex + numCards)
+}
+
+// const processCardsRecursive = (cards: Card[]): number => {
+//     const numberOfCards: number[] = [cards.length]
+//     console.log('INITIAL COUNT', numberOfCards, 'INPUT', cards)
+//     for (let i = 0; i < cards.length; i++) {
+//         const matches = matchesOnCard(cards[i])
+
+//         if (matches) {
+//             console.log('MATCHES', matches, 'CARDNO', cards[i].cardNo)
+//             // if (i+1<cards.length) {
+//             const recursiveCount = processCardsRecursive(getNextCards(cards, i, matches))
+//             numberOfCards.push(recursiveCount)
+//             // }
+//             // else {
+//             //     numberOfCards.push(1)
+//             // }
+
+//         }
+//     }
+//     console.log('FINAL COUNT', numberOfCards)
+//     return numberOfCards.reduce(sum)
+// }
+
+const processCardsIterative = (cards: Card[][]): number => {
+    let totalNumberOfCards = 0
+    for (let i = 0; i < cards.length; i++) {
+        for (let j = 0; j < cards[i].length; j++) {
+            totalNumberOfCards++
+
+            const matches = matchesOnCard(cards[i][j])
+
+            for (let m = 0; m < matches; m++) {
+                if (i + m + 1 < cards.length) {
+                    cards[i + m + 1].push(cards[i + m + 1][0])
+                }
+            }
+        }
+    }
+    return totalNumberOfCards
 }
 
 const a = (fileName: string): number => {
     return loadFile(fileName).split("\r\n").map(rowToCard).map(pointsOnCard).reduce(sum)
-
 }
+
 const b = (fileName: string): number => {
-    return 0
+    const initialCards: Card[] = loadFile(fileName).split("\r\n").map(rowToCard)
+    //return processCardsRecursive(initialCards)
+
+    return processCardsIterative(initialCards.map(c => [c]))
 }
 
 console.log('RESULT 04A: ', a('04/input.txt'))
-// console.log('RESULT 04B: ', b('04/input.txt'))
+console.log('RESULT 04B: ', b('04/input.txt'))
 
 export {
     a,
     b,
     rowToCard,
-    pointsOnCard
+    pointsOnCard,
+    matchesOnCard,
+    getNextCards
 }
